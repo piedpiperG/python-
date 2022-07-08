fa1e774f4444f82562691c16a89fbc5fc6ba98c4
piedpiperG <3135484689@qq.com>
2022-07-08 09:38:41 +0800

    7.8提交

---
+++ format字符串.py
@@ -0,0 +1,4 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"     #注意用大括号
+print(full_name)
---
+++ 字符串大小写.py
@@ -0,0 +1,4 @@
+name = "ada lovelAce"
+print(name.title()) #只有首字母大写
+print(name.lower()) #全部小写
+print(name.upper()) #全部大写
